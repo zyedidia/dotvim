bff3063c0bac39ad69b14a9080faba5ed18b87d9
Zachary Yedidia <zyedidia@gmail.com>
2015-06-06 13:26:07 -0400

    Add ctrlp and update plugins

--- bundle/julialint/autoload/lintrequest.jl
+++ bundle/julialint/autoload/lintrequest.jl
@@ -2,6 +2,6 @@ cmd = "bash"
 file = dirname(Base.source_path()) * "/lintrequest.sh"
 arg = ARGS[1]
 if isfile(".julialint")
-	arg = strip(readlines(".julialint")[1], "\n")
+	arg = chomp(readlines(".julialint")[1])
 end
 run(`$cmd $file $arg`)
--- bundle/julialint/autoload/lintrequest.sh
+++ bundle/julialint/autoload/lintrequest.sh
@@ -1,1 +1,1 @@
-(echo "$1"; sleep 0.01) | nc -w 2 localhost 2222
+(echo "$1"; sleep 1) | nc -w 2 localhost 2222
